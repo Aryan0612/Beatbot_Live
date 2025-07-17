107927804f74595c17eff9e4581f799d6a7c0b0b
POATs <124173859+POATs@users.noreply.github.com>
2025-07-18 04:42:22 +0530

    Changed login url authh

--- spotify-clone-main/client/src/Login.js
+++ spotify-clone-main/client/src/Login.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Container } from "react-bootstrap"
 
 const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=7d711f19e40c4f49886786aea6919a45&response_type=code&redirect_uri=https://beatbot-live.vercel.app/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+  "https://accounts.spotify.com/authorize?client_id=7d711f19e40c4f49886786aea6919a45&response_type=code&redirect_uri=https://beatbot_live-spotify.vercel.app/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
 
 export default function Login() {
